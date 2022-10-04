6b62a58acf421571cfa40007bed1c66a94d6b316
Geovani29 <pgeovani@uninorte.edu.co>
2022-10-04 13:31:32 +0000

    commit

--- map.py
+++ map.py
@@ -2,7 +2,7 @@ import folium
 
 m = folium.Map(location=[42.3601, -71.8589], zoom_start=12)
 tooltip = 'Click for more ingo'
-folium.Marker([42.363600, -71.099500], 
+folium.Marker([-74.90753173828125, 10.271681232946728], 
               popup='<strong>Location One</strong>',
               tooltip = tooltip).add_to(m)
 
