1693c512005ec535aeef02fe6e027c8939ec565c
Geovani29 <pgeovani@uninorte.edu.co>
2022-10-05 01:21:19 +0000

    commit

--- map.py
+++ map.py
@@ -1,10 +1,8 @@
-import folium
-
-m = folium.Map(location=[10.916583786068093, -74.77432250976562, zoom_start=12)
-tooltip = 'soledad'
-folium.Marker([10.916583786068093, -74.77432250976562], 
-              popup='<strong>Location One</strong>',
-              tooltip = tooltip).add_to(m)
-
-
-m.save('map.html')
+import folium 
+class map:
+    m = folium.Map(location=[10.916583786068093, -74.77432250976562], zoom_start=12)
+    tooltip = 'soledad'
+    folium.Marker([10.916583786068093, -74.77432250976562], 
+                popup='<strong>Location One</strong>',
+                tooltip = tooltip).add_to(m)
+    m.save('map.html')
