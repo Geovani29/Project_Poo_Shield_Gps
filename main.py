40d4f09f38e226d9b3dd65363415eb0d094a2180
Laureano55 <110035786+Laureano55@users.noreply.github.com>
2022-10-05 16:25:17 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -3,4 +3,5 @@ from map import map
 map = map
 
 
-map.draw_map("la vida",-74.85095858573912,11.017804821105223)
+map.draw_map("universidad",11.017804821105223,-74.85095858573912,
+             "casa",10.9089565752303,-74.7934627532959)
--- map.py
+++ map.py
@@ -3,12 +3,16 @@ class map:
     def __init__(self):
         pass
 
-    def draw_map(x,lat,lon):
+    def draw_map(x1,lat1,lon1,x2,lat2,lon2):
         
-        m = folium.Map(location=[lat, lon], zoom_start=12)
-        tooltip = x
-        folium.Marker([lat, lon], 
+        m = folium.Map(location=[lat1, lon1], zoom_start=12)
+        tooltip1 = x1
+        tooltip2 = x2
+        folium.Marker([lat1, lon1], 
                     popup='<strong>Location One</strong>',
-                    tooltip = tooltip).add_to(m)
+                    tooltip = tooltip1).add_to(m)
+        folium.Marker([lat2, lon2], 
+                    popup='<strong>Location One</strong>',
+                    tooltip = tooltip2).add_to(m)
         m.save('map.html')
         return 
