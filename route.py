bc66951859236d121857bd4dc3103e919e2adca5
Geovani29 <pgeovani@uninorte.edu.co>
2022-10-05 03:18:05 +0000

    commit

--- route.py
+++ route.py
@@ -11,8 +11,8 @@ class route:
         
         ox.config(log_console=True, use_cache=True)
         # define the start and end locations in latlng
-        start_latlng = (37.78497,-122.43327)
-        end_latlng = (37.78071,-122.41445)
+        start_latlng = (10.271681232946728, -74.90753173828125)
+        end_latlng = (10.371660215460267, -75.0311279296875)
         # location where you want to find your route
         place     = 'San Francisco, California, United States'
         # find shortest route based on the mode of travel
@@ -34,4 +34,4 @@ class route:
 
         shortest_route_map = ox.plot_route_folium(graph, shortest_route)
         shortest_route_map
-        pass
+        
