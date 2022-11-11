af6b5c3ca7c783140f1de17450b4678056a01208
Geovani29 <pgeovani@uninorte.edu.co>
2022-11-11 01:50:42 +0000

    Code to run

--- main.py
+++ main.py
@@ -5,3 +5,28 @@ key = "TPAGE37FU5xR5kvC7LPAeN3snKSUlv0B"
 
 api = API(api_url, key)
 ruta = Ruta
+
+while True:
+    
+    pedido = direction
+        
+    pedido.inicio = input("Ingresa el origen: ")
+
+    if pedido.inicio == 'q':
+        break
+    
+    pedido.final = input("Ingresa el destino: ")
+    
+    if pedido.final == 'q':
+        break    
+    
+    
+    api.get_url(pedido)
+    
+    api.get_json()
+    #el json se guardó en el atributo api.jason_data
+    
+    api.check_code()
+
+    ruta.print(pedido, api)
+    
